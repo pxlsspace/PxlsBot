297e465db04f73601b9823f689e60396fb3664d0
Andrew Black <andrewblack2656@gmail.com>
2019-08-15 14:55:28 +0200

    Add database support
    This commit adds basic database support.

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+const mariadb = require('mariadb');
+
 const logger = require('./logger');
 const { getEvents } = require('./utils');
 
 const config = require('./config');
+
+/**
+ * A database connection pool, set during initialization.
+ * @property {mariadb.Pool}
+ */
+let database;
+
+/**
+ * Gets the database pool.
+ * @returns {mariadb.Pool}
+ */
+function getDatabase () {
+  return database;
+}
 
 /**
  * An array of event objects, set during initialization.
@@ -26,6 +42,17 @@ async function main () {
   logger.info('PxlsBot 1.0.0');
   if (typeof config.token === 'undefined') {
     logger.fatal('`token` missing in config');
+  }
+  database = mariadb.createPool(config.database);
+  logger.debug('Testing database configuration...');
+  try {
+    const connection = await database.getConnection();
+    await connection.ping();
+    await connection.end();
+    logger.debug('Database successfully pinged.');
+  } catch (err) {
+    logger.error(err);
+    logger.fatal('Could not initialize database connection.');
   }
   logger.info('Initializing events...');
   events = await getEvents(config.eventsPath);
@@ -53,4 +80,10 @@ process.on('SIGINT', exit);
 
 main();
 
-module.exports = { client, events, login, exit };
+module.exports = {
+  client,
+  getDatabase,
+  events,
+  login,
+  exit
+};
