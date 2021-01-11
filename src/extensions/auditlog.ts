pxlsspace/PxlsBot

commit 25b48eb184c7d924f6fcfe01887dac71223ef916

Reorganize project to mimick discord.py's extension functionality (#16)
* Rename config.json.example to config.example.json
This should make it easier for IDEs to detect and highlight the file based on the extension.

* Reorganize project to mimick discord.py's extension functionality.
- Merges events and commands into a single concept: extensions. These can be loaded and unloaded at will, and are based on files with an exported setup() function.
  - All commands have been made into extensions.
  - Some events have been moved to index.ts
- Makes the Discord Client object the center of truth. Extends Discord.js's Client to hold loaded extensions and registered commands.
- Adds a new Context object, taken from discord.py, which holds fields related to the command execution context (message, command executed, argument specified, etc.)
  - Easily expandible.
- Moves database logic to its own file.

Also:
- Makes TypeScript target ES2020 so we can make use of the latest EcmaScript features.

src/extensions/auditlog.ts
@@ -1,11 +1,11 @@
 import * as Discord from 'discord.js';
 import * as pg from 'pg';
 
-import { getDatabase } from '../index';
-import { Command } from '../command';
+import { Client } from '../client';
+import * as database from '../database';
+import { Command, Context } from '../command';
 import * as logger from '../logger';
-import { getCommands, Color, truncate } from '../utils';
-import * as config from '../config';
+import { Color, truncate } from '../utils';
 
 type DatabaseAuditLog = {
   /* eslint-disable camelcase */
@@ -17,8 +17,6 @@ type DatabaseAuditLog = {
   timestamp: Date
   /* eslint-enable camelcase */
 }
-
-const database = getDatabase();
 
 let commands: Command[];
 
@@ -48,34 +46,13 @@ export async function insertAuditLog(connection: pg.PoolClient, message: Discord
   }
 }
 
-async function init() {
-  commands = await getCommands(config.get('commandsPath', 'commands'));
-  try {
-    const connection = await database.connect();
-    await connection.query(`
-      CREATE TABLE IF NOT EXISTS auditlog (
-        id SERIAL NOT NULL PRIMARY KEY,
-        guild_id VARCHAR(18) NOT NULL,
-        user_id VARCHAR(18) NOT NULL,
-        command_id TEXT,
-        message TEXT,
-        timestamp TIMESTAMP NOT NULL DEFAULT NOW()
-      )
-    `);
-    connection.release();
-  } catch (err) {
-    logger.error('Could not insert "auditlog" table.');
-    logger.fatal(err);
-  }
-}
-
-async function execute(client: Discord.Client, message: Discord.Message): Promise<void> {
+async function execute({ client, message }: Context): Promise<void> {
   const args = message.content.split(' ');
   const embed = new Discord.MessageEmbed();
   embed.setColor(Color.rainbow.skyblue.toColorResolvable());
   if (args.length < 2) {
     try {
-      const connection = await database.connect();
+      const connection = await database.getConnection();
       const result = await connection.query(`
         SELECT
           *
@@ -133,7 +110,7 @@ async function execute(client: Discord.Client, message: Discord.Message): Promis
   }
   const id = parseInt(args[1]);
   try {
-    const connection = await database.connect();
+    const connection = await database.getConnection();
     const result = await connection.query(`
       SELECT
         *
@@ -178,5 +155,26 @@ export const command = new Command({
   serverOnly: true,
   permissions: Discord.Permissions.FLAGS.MANAGE_GUILD
 });
-command.init = init;
 command.execute = execute;
+
+export async function setup(client: Client): Promise<void> {
+  try {
+    const connection = await database.getConnection();
+    await connection.query(`
+      CREATE TABLE IF NOT EXISTS auditlog (
+        id SERIAL NOT NULL PRIMARY KEY,
+        guild_id VARCHAR(18) NOT NULL,
+        user_id VARCHAR(18) NOT NULL,
+        command_id TEXT,
+        message TEXT,
+        timestamp TIMESTAMP NOT NULL DEFAULT NOW()
+      )
+    `);
+    connection.release();
+  } catch (err) {
+    logger.error('Could not insert "auditlog" table.');
+    logger.fatal(err);
+  }
+
+  client.registerCommand(command);
+}

src/client.ts
@@ -1,0 +1,166 @@
+import * as Discord from 'discord.js';
+
+import { Command, Context } from './command';
+import * as logger from './logger';
+import * as config from './config';
+import { readOnlyViewOf, splitN } from './utils';
+
+export type ExtensionFile = {
+  // eslint-disable-next-line no-use-before-define
+  setup(bot: Client): Promise<void> | void
+  // eslint-disable-next-line no-use-before-define
+  teardown?(bot: Client): Promise<void> | void
+}
+
+export abstract class PrefixHandler {
+  abstract get(message: Discord.Message): string;
+}
+
+export class Client extends Discord.Client {
+  #commands: Command[];
+  #extensions: Record<string, ExtensionFile>;
+
+  get commands(): Command[] {
+    return readOnlyViewOf(this.#commands);
+  }
+
+  get extensions(): Record<string, ExtensionFile> {
+    return readOnlyViewOf(this.#extensions);
+  }
+
+  constructor(options: Discord.ClientOptions) {
+    super(options);
+
+    this.#extensions = {};
+    this.#commands = [];
+    this.on('message', this.handleMessageCommand.bind(this));
+  }
+
+  /**
+   * Function used to find if and where the command prefix ends on a message.
+   * @param message
+   * @returns {Promise<number> | number} the index at which the prefix ends,
+   * or -1 if no prefix was found.
+   */
+  getCommandPrefixOffset(message: Discord.Message): Promise<number> | number {
+    const prefix = config.get('prefix', '!');
+    const start = message.content.indexOf(prefix);
+    return start === -1 ? -1 : start + prefix.length;
+  }
+
+  private async handleMessageCommand(message: Discord.Message) {
+    if (message.author.bot) {
+      return;
+    }
+    const prefixOffset = await this.getCommandPrefixOffset(message);
+    if (prefixOffset === -1) {
+      return;
+    }
+    const [cmd, args] = splitN(message.content.substring(prefixOffset), ' ', 1);
+    let match: Command;
+    for (const command of this.#commands) {
+      if (command.aliases.includes(cmd)) {
+        match = command;
+      }
+    }
+    if (!match) {
+      return;
+    }
+    if (match.serverOnly && !message.guild) {
+      await message.channel.send('This command may only be run in a guild.');
+      return;
+    }
+    if (!match.hasPermission(message.member)) {
+      logger.debug(`${message.author.tag} attempted to execute command "${match.name}" in guild "${message.guild.name}" without permission.`);
+      await message.channel.send('You do not have permission to run this command.');
+      return;
+    }
+    logger.debug(`${message.author.tag} is executing command "${match.name}" in guild "${message.guild.name}".`);
+    void message.channel.startTyping();
+    await match.execute(new Context(message, match, args));
+    message.channel.stopTyping(true);
+  }
+
+  isCommandRegistered(command: Command): boolean {
+    return this.#commands.some((cmd) =>
+      cmd.name === command.name ||
+      cmd.aliases.some((cmdAlias) => command.name === cmdAlias || command.aliases.includes(cmdAlias))
+    );
+  }
+
+  getCommandByName(nameOrAlias: string): Command | null {
+    for (const cmd of this.#commands) {
+      if (cmd.name === nameOrAlias || cmd.aliases.includes(nameOrAlias)) {
+        return cmd;
+      }
+    }
+
+    return null;
+  }
+
+  registerCommand(command: Command): void {
+    if (this.isCommandRegistered(command)) {
+      throw new Error(`attempted to register command ${command.name} but a command with the same name or alias was already registered.`);
+    }
+    this.#commands.push(command);
+  }
+
+  unregisterCommand(commandOrName: Command | string): void {
+    const cmd = commandOrName instanceof Command
+      ? commandOrName
+      : this.getCommandByName(commandOrName);
+    const idx = this.#commands.indexOf(cmd);
+    if (idx === -1) {
+      throw new Error(`attempted to unregister unknown command ${cmd.name}`);
+    }
+
+    this.#commands.splice(idx, 1);
+  }
+
+  private async safeTeardownExt(extFile: ExtensionFile) {
+    if (typeof extFile.teardown === 'function') {
+      return await extFile.teardown(this);
+    }
+  }
+
+  getExtensionByName(name: string): ExtensionFile | null {
+    return this.#extensions[name] ?? null;
+  }
+
+  async reloadExtension(name: string): Promise<ExtensionFile> {
+    return this.loadExtension(name);
+  }
+
+  async loadExtension(path: string): Promise<ExtensionFile> {
+    // Remove file from require cache, if its there.
+    delete require.cache[path];
+
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
+    const extFile = require(path) as ExtensionFile;
+    if (typeof extFile.setup !== 'function') {
+      throw new Error(`extension at ${path} doesn't export a setup function.`);
+    }
+
+    this.#extensions[path] = extFile;
+    await extFile.setup(this);
+    return extFile;
+  }
+
+  async unloadExtension(path: string): Promise<void> {
+    // Remove file from require cache, if its there.
+    delete require.cache[path];
+
+    const extFile = this.#extensions[path];
+    if (extFile == null) {
+      throw new Error(`attempted to unload extension ${path} which was never loaded.`);
+    }
+
+    delete this.#extensions[path];
+    return this.safeTeardownExt(extFile);
+  }
+
+  async destroy(): Promise<void> {
+    super.destroy();
+    await Promise.all(Object.values(this.#extensions).map(this.safeTeardownExt.bind(null)));
+  }
+}

src/command.ts
@@ -1,5 +1,7 @@
 import * as Discord from 'discord.js';
 
+import { Client } from './client';
+
 interface CommandData {
   id: string;
   name: string;
@@ -12,17 +14,17 @@ interface CommandData {
 }
 
 export class Command implements CommandData {
-  public id: string;
-  public name: string;
-  public category: string;
-  public description: string;
-  public usage: string;
-  public aliases: string[];
-  public serverOnly = false;
-  public permissions = 0;
+  readonly id: string;
+  readonly name: string;
+  readonly category: string;
+  readonly description: string;
+  readonly usage: string;
+  readonly aliases: string[];
+  readonly serverOnly?: boolean;
+  readonly permissions?: number;
 
-  init: () => Promise<void>;
-  execute: (client: Discord.Client, message: Discord.Message) => void | Promise<void>;
+  // eslint-disable-next-line no-use-before-define
+  execute: (context: Context) => Promise<void> | void;
 
   constructor(data: CommandData) {
     this.id = data.id;
@@ -41,5 +43,21 @@ export class Command implements CommandData {
    */
   hasPermission(member: Discord.GuildMember): boolean {
     return member.hasPermission(this.permissions);
+  }
+}
+
+export class Context {
+  message: Discord.Message;
+  command: Command;
+  argument: string;
+
+  get client(): Client {
+    return this.message.client as Client;
+  }
+
+  constructor(message: Discord.Message, command: Command, argument: string) {
+    this.message = message;
+    this.command = command;
+    this.argument = argument;
   }
 }

src/commands/help.ts
@@ -1,84 +1,0 @@
-import * as Discord from 'discord.js';
-
-import { getDatabase } from '../index';
-import { Command } from '../command';
-import { getCommands, Color } from '../utils';
-import * as config from '../config';
-
-import { getPrefix } from './config';
-
-const database = getDatabase();
-
-/**
- * An array of commands, set during initialization.
- * @property {Command[]} commands The commands.
- */
-let commands: Command[];
-
-/**
- * An array of all the categories used by commands.
- * @property {Object.<string, Command>} categories The categories.
- */
-const categories: { [category: string]: Command[] } = {};
-
-async function init() {
-  commands = await getCommands(config.get('commandsPath', 'commands'));
-  commands.forEach(command => {
-    if (categories[command.category] == null) {
-      categories[command.category] = [];
-    }
-    categories[command.category].push(command);
-  });
-}
-
-async function execute(client: Discord.Client, message: Discord.Message): Promise<void> {
-  const args = message.content.split(' ');
-  const embed = new Discord.MessageEmbed();
-  embed.setColor(Color.rainbow.skyblue.toColorResolvable());
-  if (args.length < 2) {
-    // (prefix)help
-    const categoryNames = Object.keys(categories);
-    for (const category of categoryNames) {
-      const categoryCommands = categories[category];
-      const commandList = categoryCommands.map(cmd => cmd.name);
-      embed.addField(category, commandList);
-    }
-    const helpText = `For more information on a specific command, try \`${config.get('prefix')}help [command]\`.`;
-    embed.setDescription(helpText);
-  } else {
-    // (prefix)help [command name/alias]
-    const command = commands.find(command => {
-      return args.slice(1).join(' ').toLowerCase() === command.name.toLowerCase() ||
-      command.aliases.includes(args.slice(1).join(' ').toLowerCase());
-    });
-    if (typeof command === 'undefined') {
-      await message.channel.send('Could not a find command with the name or alias "' + args[1] + '".');
-      return;
-    }
-    const permissions = new Discord.Permissions(command.permissions);
-    let prefix = '';
-    const connection = await database.connect();
-    prefix = await getPrefix(connection, message.guild.id);
-    connection.release();
-    const helpText = `
-      **Description:** ${command.description}
-      **Usage:** \`${prefix}${command.usage}\`
-      **Aliases:** [ \`${command.aliases.join('` | `')}\` ]
-      **Required Permissions:** ${command.permissions}
-      ${permissions.toArray(false).map(v => ' - `' + v + '`').join('\n')}
-    `;
-    embed.addField(command.name, helpText);
-  }
-  await message.channel.send(embed);
-}
-
-export const command = new Command({
-  id: 'help',
-  name: 'Help',
-  category: 'Utility',
-  description: 'Returns a list of commands, or if specified, information about a specific command.',
-  usage: 'help [item identifier]',
-  aliases: ['help', '?']
-});
-command.init = init;
-command.execute = execute;

src/database.ts
@@ -1,0 +1,31 @@
+import * as pg from 'pg';
+
+/**
+ * A database connection pool, set during initialization.
+ * @property {pg.Pool}
+ */
+export let database: pg.Pool;
+
+/**
+ * Gets the database pool.
+ * @returns {pg.Pool}
+ */
+export function getPool(): pg.Pool {
+  return database;
+}
+
+/**
+ * Gets a database pool connection.
+ * @returns {pg.Pool}
+ */
+export function getConnection(): Promise<pg.PoolClient> {
+  return database.connect();
+}
+
+/**
+ * Initializes the database pool.
+ * @param config {pg.PoolConfig}
+ */
+export function init(config: pg.PoolConfig): void {
+  database = new pg.Pool(config);
+}

src/events/error.ts
@@ -1,20 +1,0 @@
-import { login } from '../index';
-import * as logger from '../logger';
-import * as config from '../config';
-
-export const name = 'error';
-
-/**
- * Executed whenever a discord.js WebSocket error occurs.
- * @param {error} err The error.
- */
-export function execute(err: Error): void {
-  if (err.name === 'ENOTFOUND') {
-    logger.error('Lost connection to Discord, will attempt reconnect in 30 seconds...');
-    setTimeout(() => {
-      void login();
-    }, config.get('reconnectTimeout'));
-    return;
-  }
-  logger.error(err);
-}

src/events/message-coordinates.ts
@@ -1,22 +1,0 @@
-import * as Discord from 'discord.js';
-
-import { validateCoordinates } from '../commands/coordinates';
-import * as config from '../config';
-
-const coordsRegex = /\(([0-9]+)[., ]{1,2}([0-9]+)[., ]{0,2}([0-9]+)?x?\)/i;
-
-export const name = 'message';
-
-/**
- * Executed whenever a message is received over the WebSocket.
- * @param {Discord.Message} message The message.
- */
-export async function execute(message: Discord.Message): Promise<void> {
-  if (message.author.bot) {
-    return;
-  }
-  const exec = coordsRegex.exec(message.content);
-  if (exec && validateCoordinates(exec[1], exec[2], exec[3])) {
-    await message.channel.send(`<${config.getGameURL()}/#x=${exec[1]}&y=${exec[2]}&scale=${exec[3] ?? '20'}>`);
-  }
-}

src/events/message.ts
@@ -1,82 +1,0 @@
-import * as Discord from 'discord.js';
-
-import { client, getDatabase } from '../index';
-import * as logger from '../logger';
-import { getCommands } from '../utils';
-import { Command } from '../command';
-import * as config from '../config';
-
-import { getPrefix } from '../commands/config';
-
-const database = getDatabase();
-
-/**
- * An array of commands, set during initialization.
- * @property {Command[]} commands The commands.
- */
-let commands: Command[];
-
-export const name = 'message';
-
-export async function init(): Promise<void> {
-  logger.info('Initializing commands...');
-  commands = await getCommands(config.get('commandsPath', 'commands'));
-
-  for (const command of commands) {
-    if (typeof command.init !== 'undefined') {
-      await command.init();
-    }
-  }
-}
-
-/**
- * Executed whenever a message is received over the WebSocket.
- * @param {Discord.Message} message The message.
- */
-export async function execute(message: Discord.Message): Promise<void> {
-  if (message.author.bot) {
-    return;
-  }
-  const args = message.content.split(' ');
-  let prefix: string;
-  try {
-    const connection = await database.connect();
-    if (typeof message.guild === 'undefined') {
-      prefix = config.get('prefix');
-    } else {
-      prefix = await getPrefix(connection, message.guild.id);
-    }
-    connection.release();
-  } catch (err) {
-    logger.error('Could not get prefix from database.');
-    logger.fatal(err);
-  }
-  if (args[0].toLowerCase().startsWith(prefix)) {
-    const cmd = args[0].toLowerCase().replace(prefix, '');
-    let match: Command;
-    for (const command of commands) {
-      if (command.aliases.includes(cmd)) {
-        match = command;
-      }
-    }
-    if (!match) {
-      return;
-    }
-    if (match.serverOnly && !message.guild) {
-      await message.channel.send('This command may only be run in a guild.');
-      return;
-    }
-    if (!match.hasPermission(message.member)) {
-      logger.debug(`${message.author.tag} attempted to execute command "${match.name}" in guild "${message.guild.name}" without permission.`);
-      await message.channel.send('You do not have permission to run this command.');
-      return;
-    }
-    logger.debug(`${message.author.tag} is executing command "${match.name}" in guild "${message.guild.name}".`);
-    await message.channel.startTyping();
-    const result = match.execute(client, message);
-    if (result instanceof Promise) {
-      await result;
-    }
-    message.channel.stopTyping(true);
-  }
-}

commit 8b4c5a53fd8b9ce9956bff5d5ee782d49a5c3807

Fix a crash on !auditlog introduced by PR #16

src/extensions/auditlog.ts
@@ -18,8 +18,6 @@ type DatabaseAuditLog = {
   /* eslint-enable camelcase */
 }
 
-let commands: Command[];
-
 /**
  * Inserts an audit log entry.
  * @param {pg.PoolClient} connection The database connection.
@@ -71,7 +69,7 @@ async function execute({ client, message }: Context): Promise<void> {
       const formattedArr: string[] = [];
       for (const auditLog of <DatabaseAuditLog[]> result.rows) {
         const user = await client.users.fetch(auditLog.user_id);
-        const command = commands.find(cmd => cmd.id === auditLog.command_id);
+        const command = client.commands.find(cmd => cmd.id === auditLog.command_id);
         const commandID = command.id ?? auditLog.command_id;
         const timestamp = logger.getDateTime(auditLog.timestamp, true);
         formattedArr.push(`
@@ -130,7 +128,7 @@ async function execute({ client, message }: Context): Promise<void> {
     }
     const auditLog = result.rows[0] as DatabaseAuditLog;
     const user = await client.users.fetch(auditLog.user_id);
-    const command = commands.find(cmd => cmd.id === auditLog.command_id);
+    const command = client.commands.find(cmd => cmd.id === auditLog.command_id);
     const commandID = command.id ?? auditLog.command_id;
     embed.setTitle(`Audit Log #${id}`);
     embed.setAuthor(`${user.tag} (${user.id})`, user.displayAvatarURL({ dynamic: true }));